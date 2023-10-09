661097d42b6979788bc7f8d4807e29354066ce9b
Luis Rafael Segura <lrseguramartinez@gmail.com>
2023-10-08 23:56:07 -0400

    Adding files

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,78 @@
+'use client'
+import React from "react";
+
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    Link,
+    NavbarMenu,
+    NavbarMenuToggle
+} from "@nextui-org/react";
+import {NavbarMenuItem} from "@nextui-org/navbar";
+import DropdownItemsNavigation from "@/components/DropdownItemsNavigation";
+
+
+export default function Navigation() {
+    // const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    //
+    // const menuItems = [
+    //     "Profile",
+    //     "Dashboard",
+    //     "Activity",
+    //     "Analytics",
+    //     "System",
+    //     "Deployments",
+    //     "My Settings",
+    //     "Team Settings",
+    //     "Help & Feedback",
+    //     "Log Out",
+    // ];
+
+    return (
+        <Navbar >
+            <NavbarContent>
+                <NavbarMenuToggle
+                    className="sm:hidden"
+                />
+                <NavbarBrand>
+                    <Link color={"foreground"} href={"/"} aria-current="page">
+                        <p className="font-bold text-inherit">Test</p>
+                    </Link>
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        <DropdownItemsNavigation/>
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive={false}>
+                    <Link color={"foreground"} href="/customers" aria-current="page">
+                        Customers
+                    </Link>
+                </NavbarItem>
+
+            </NavbarContent>
+
+            {/*<NavbarMenu>*/}
+            {/*    {menuItems.map((item, index) => (*/}
+            {/*        <NavbarMenuItem key={`${item}-${index}`}>*/}
+            {/*            <Link*/}
+            {/*                color={*/}
+            {/*                    index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"*/}
+            {/*                }*/}
+            {/*                className="w-full"*/}
+            {/*                href="#"*/}
+            {/*                size="lg"*/}
+            {/*            >*/}
+            {/*                {item}*/}
+            {/*            </Link>*/}
+            {/*        </NavbarMenuItem>*/}
+            {/*    ))}*/}
+            {/*</NavbarMenu>*/}
+        </Navbar>
+    );
+}
